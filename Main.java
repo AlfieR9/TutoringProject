11c0287fea324972e311cb8ce372d95b8c7cca7c
AlfieR9 <alfie_rodbert@yahoo.co.uk>
2021-05-17 17:36:41 +0100

    Second Commit

--- Main.java
+++ Main.java
@@ -1,7 +1,34 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //Create an array of 10 numbers,
+        //Ask the user for 10 numbers.
+        //Return the average
+
+        //What if the user doesnt enter a number?
+        //How can we get decimals? if so can we round them to 2 dp?
+        //Can we ask the user if they want to use the program again? Otherwise we exit.
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Enter 10 numbers");
+
+        int[] numbers = new int[10];
+        int total = 0;
+        int average;
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = keyboard.nextInt();
+        }
+        for (int a = 0; a < numbers.length; a++) {
+            total += numbers[a];
+        }
+
+        System.out.println("This is the average: " + total / numbers.length);
+
     }
 
 }
